47eef65e0d5517698b093ec87a71b812b1fec913
Ayush <ayusha2@tepper.cmu.edu>
2022-04-13 03:41:21 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # msba_schain_22
+
+This Project is to model a simulation of Warehouse based operations.
+
+# Git link
+
+
+# Multiple repos
+
+
+# Project Structure
+
+- Master Controller
+
+-  Simulation Controller
+    -  multiple iterations with different variables
+
+- Stage Controller
---
+++ constants.py
@@ -0,0 +1,55 @@
+""""
+Storing Constants
+"""
+
+# P1 tshirts
+P1_PROFIT         = 0
+P1_PENALTY        = 0
+P1_HOLDING_COST   = 0
+P1_WEIGHT         = 0
+
+# P2 Hoodie       
+P2_PROFIT         = 0
+P2_PENALTY        = 0
+P2_HOLDING_COST   = 0
+P2_WEIGHT         = 0
+
+# P3 Sweatpants   
+P3_PROFIT         = 0
+P3_PENALTY        = 0
+P3_HOLDING_COST   = 0
+P3_WEIGHT         = 0
+
+# P4 Sneaker      
+P4_PROFIT         = 0
+P4_PENALTY        = 0
+P4_HOLDING_COST   = 0
+P4_WEIGHT         = 0
+
+# Workes
+LABOR_RATES       = 22.5
+
+# Stage 1
+DELIVERY_COST     = 1000
+DELIVERY_COST_WEEKLY    = 2600000
+DELIVERY_COST_DAILY     = 3600000
+DELIVERY_PENALTY_WEEKLY = 50000
+DELIVERY_PENALTY_DAILY  = 10000
+
+# Stage 2
+# Stage 3
+# Stage 4
+# Stage 5
+
+
+
+
+
+
+
+
+
+
+
+
+
